9675644b1826fa571c6823d254f7fb3254c0c44b
antonsubang0 <65964326+antonsubang0@users.noreply.github.com>
2021-10-01 09:35:23 +0700

    Create sortingMultiParameters.js

---
+++ sortingMultiParameters.js
@@ -0,0 +1,45 @@
+const karyawan = [
+  {nama: 'Anton', bagian: 'Hatchbrood'},
+  {nama: 'Yosan', bagian: 'Pullchick'},
+  {nama: 'Eko', bagian: 'Hatcher'},
+  {nama: 'Karyamin', bagian: 'Hatcher'},
+  {nama: 'Susi', bagian: 'Hatchbrood'},
+  {nama: 'Bowo', bagian: 'Pullchick'},
+];
+
+const urutkan = () => {
+  return karyawan.sort((a,b) => {
+    if (a.bagian === b.bagian) {
+      if (a.nama < b.nama){
+        return -1
+      } else if (a.nama > b.nama){
+        return +1
+      } else {
+        return 0
+      }
+    } else {
+      if (a.bagian < b.bagian){
+        return -1
+      } else if (a.bagian > b.bagian){
+        return +1
+      } else {
+        return 0
+      }
+    }
+
+  })
+}
+
+const urutkan1 = () => {
+  return karyawan.sort(function (a, b) {
+    return a.bagian.localeCompare(b.bagian) || b.nama - a.nama;
+  });
+}
+
+const urutkan2 = () => {
+  return karyawan.sort((a, b)=>{
+    return b.bagian - a.bagian || b.nama - a.nama;
+  })
+}
+
+console.log(urutkan(), urutkan1(), urutkan2())
