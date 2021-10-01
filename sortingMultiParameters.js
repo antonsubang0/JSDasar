e120ddfa8ff6b6fb15cd7433d1937b68999edec0
antonsubang0 <65964326+antonsubang0@users.noreply.github.com>
2021-10-01 09:43:25 +0700

    Sorting Js

--- sortingMultiParameters.js
+++ sortingMultiParameters.js
@@ -1,3 +1,6 @@
+import 'bootstrap@4.6.0'
+import $ from 'jquery'
+
 const karyawan = [
   {nama: 'Anton', bagian: 'Hatchbrood'},
   {nama: 'Yosan', bagian: 'Pullchick'},
@@ -6,40 +9,24 @@ const karyawan = [
   {nama: 'Susi', bagian: 'Hatchbrood'},
   {nama: 'Bowo', bagian: 'Pullchick'},
 ];
-
+// cara barbar
 const urutkan = () => {
   return karyawan.sort((a,b) => {
     if (a.bagian === b.bagian) {
-      if (a.nama < b.nama){
-        return -1
-      } else if (a.nama > b.nama){
-        return +1
-      } else {
-        return 0
-      }
+      if (a.nama < b.nama){ return -1 } else if (a.nama > b.nama){ return +1 } else { return 0 }
     } else {
-      if (a.bagian < b.bagian){
-        return -1
-      } else if (a.bagian > b.bagian){
-        return +1
-      } else {
-        return 0
-      }
+      if (a.bagian < b.bagian){ return -1 } else if (a.bagian > b.bagian){ return +1 } else { return 0 }
     }
-
   })
 }
-
+// cara yang paling umum
 const urutkan1 = () => {
   return karyawan.sort(function (a, b) {
-    return a.bagian.localeCompare(b.bagian) || b.nama - a.nama;
-  });
+    return a.bagian.localeCompare(b.bagian) || b.nama - a.nama });
 }
-
+//cara coba gila
 const urutkan2 = () => {
-  return karyawan.sort((a, b)=>{
-    return b.bagian - a.bagian || b.nama - a.nama;
-  })
+  return karyawan.sort((a, b)=>{ return b.bagian - a.bagian || b.nama - a.nama })
 }
 
 console.log(urutkan(), urutkan1(), urutkan2())
